929cd462ebb7a6b96286c2d00b78f3f99c2df29e
RiaGohel <riagohel@outlook.com>
2021-09-04 16:01:31 +0530

    Added left side content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: "75%",
     width: "65%",
     height: "100%",
+    boxShadow: "0 1px 8px 0 rgb(0 0 0 / 8%)",
   },
   gridroot: {
     flexGrow: 1,
@@ -37,7 +36,9 @@ const useStyles = makeStyles({
 
 export default function App() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+  let name = "Raj";
+  let meeting_time = 30;
+  let prompt_message = "Web conferencing details provided upon confirmation";
 
   return (
     <Grid
@@ -61,7 +62,58 @@ export default function App() {
                     "1px solid var(--text-color-level3, rgba(26, 26, 26, 0.1))",
                 }}
               >
-                <p>Left Side</p>
+                <div>
+                  <div style={{ textAlign: "left", marginLeft: "27px" }}>
+                    <h3
+                      style={{
+                        marginTop: "27px",
+                        marginBottom: "0",
+                        color: "grey",
+                      }}
+                    >
+                      {name}
+                    </h3>
+                    <h2 style={{ marginTop: "7px" }}>
+                      {meeting_time} Minute Meeting
+                    </h2>
+                    <div
+                      style={{
+                        display: "flex",
+                        color: "grey",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <WatchLaterIcon />
+                      <p
+                        style={{
+                          display: "inline-flex",
+                          marginTop: "0",
+                          marginLeft: "10px",
+                        }}
+                      >
+                        {meeting_time} mins
+                      </p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        color: "grey",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <VideocamIcon />
+                      <p
+                        style={{
+                          display: "inline-flex",
+                          marginTop: "0",
+                          marginLeft: "10px",
+                        }}
+                      >
+                        {prompt_message}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <p>Right Side</p>
